45144c7b5b03fae2df73c8d0308d0f8f7e59d03d
yaoyang <kiki@smarty.so>
2018-03-29 21:45:43 +0800

    修改 readme 测试发布

--- public/index.php
+++ public/index.php
@@ -3,6 +3,14 @@
 $repository_name = $_GET['name'];
 $repository_clone_url = $_GET['url'];
 
+if (! $repository_name) {
+    die('需要传 name 参数');
+}
+
+if (! $repository_clone_url) {
+    die('需要传 url 参数');
+}
+
 $parent_dir = '/var/www';
 $dir = $parent_dir.'/'.$repository_name;
 
