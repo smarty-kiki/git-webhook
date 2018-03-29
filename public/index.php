c282ce3a96a87799a65ed07641f675db9087a7b4
yaoyang <kiki@smarty.so>
2018-03-29 21:29:26 +0800

    修改脚本从 pust 内容中取相关的信息

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
 
-$git_url = $_GET['repo'];
-$dir_name = str_replace(['https://github.com/smarty-kiki/', '.git'], '', $git_url);
+$info = json_decode(file_get_contents('php://input'), true);
+$repository_name = $info['repository']['name'];
+$repository_clone_url = $info['repository']['clone_url'];
 
 $parent_dir = '/var/www';
-$dir = $parent_dir.'/'.$dir_name;
+$dir = $parent_dir.'/'.$repository_name;
 
-if (is_dir($dir)) {
-    exec("cd $dir; git pull origin master; git checkout -f master");
-} else {
-    exec("cd $parent_dir; git clone $git_url; cd $dir; git checkout -f master");
+if (! is_dir($dir)) {
+    exec("cd $parent_dir; git clone $repository_clone_url");
 }
+
+exec("cd $dir; git pull origin master; git checkout -f master");
